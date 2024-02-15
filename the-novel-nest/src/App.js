684da8e1396a7e82b9026fba1ffb60d79306c3f6
Renu Rao <renurao.1306@gmail.com>
2024-02-15 14:22:42 +0530

    Added data.json and modified App.js

--- the-novel-nest/src/App.js
+++ the-novel-nest/src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Booklist from './components/Booklist'
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Banner from './components/Banner';
+import RegistrationForm from './components/RegistrationForm';
+import Cart from './components/Cart';
+import Orders from './components/Orders';
+import About from './components/About';
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      {/* <Header/>
+      <Banner/>
+      <Booklist />
+      <Footer/> */}
+
+      
+        <Routes>
+          <Route path='' element={<><Header/><Banner/><Booklist/><Footer/></>} />
+          <Route path='/cart' element={<Cart/>} />
+          <Route path='/orders' element={<Orders/>} />
+          <Route path='/register' element={<RegistrationForm/>} />
+          <Route path='/about' element={<About/>} />
+        </Routes>
+      
+    </Router>
   );
 }
 
